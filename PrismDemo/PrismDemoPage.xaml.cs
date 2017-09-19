0c46882a92f58c9ce7e57e5ba521dea374e4349b
Vishal Pachori <vishalpachori25@gmail.com>
2017-09-19 14:55:17 +0530

    Initial Commit

---
+++ PrismDemo/PrismDemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDemo
+{
+    public partial class PrismDemoPage : ContentPage
+    {
+        public PrismDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
